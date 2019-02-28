bd9e4d27f9f84c621468cd988e1eda5a088d7019
Becky Giffen <44362049+beckygiffen@users.noreply.github.com>
2019-02-28 16:25:24 +0000

    initial commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+window.onload = function() {
+  currentYear();
+};
+
+function currentYear(){
+  const date = new Date();
+  const autoDate = document.querySelector('#autoDate');
+  autoDate.textContent = date.getFullYear();
+};
+function pagetop(){
+  width = "100%";
+}
+var feed = new Instafeed({
+  get: 'user',
+  userId: '11180810277',
+  accessToken: '11180810277.fbcd7ac.8ac285facbea465592e9826cafbc9d03',
+  limit: 3,
+  filter: function(image) {
+  var MAX_LENGTH = 40;
+
+  // here we create a property called "short_caption"
+  // on the image object, using the original caption
+  if (image.caption && image.caption.text) {
+    image.short_caption = image.caption.text.slice(0,30, MAX_LENGTH);
+  } else {
+    image.short_caption = "";
+  }
+
+  // ensure the filter doesn't reject any images
+  return true;
+},
+  template: '<div class="col-4"><img class="w-100" src={{image}}><hr><i<br></i><br><p>{{model.short_caption}}...<a href="{{link}}" target="_blank">Read More</a></p></div>'
+});
+feed.run();
